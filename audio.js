cec99723358334d4e2490ce4a49b19925281335f
davitghazaryan2002-pixel <davit.ghazaryan2002@tumo.org>
2026-07-11 15:29:16 +0400

    add music

---
+++ audio.js
@@ -0,0 +1,63 @@
+let data = {
+    title: [
+        "ANDRXRXSSO & BLOODTHXRN - DARKSIDE (1)",
+        "ANDRXRXSSO & BLOODTHXRN - DARKSIDE",
+        "phonk killazz & MantiCxrs - ALL I WANT IS YOU",
+        "phonk killazz & MantiCxrs - MONTAGEM MASENTAD"
+
+
+    ],
+
+    song: [
+        "music/ANDRXRXSSO & BLOODTHXRN - DARKSIDE (1).mp3",
+        "music/ANDRXRXSSO & BLOODTHXRN - DARKSIDE.mp3",
+        "music/Phonk Killazz & MantiCxrs - Holding Out For A Hero Hoodtrap.mp3",
+        "music/phonk killazz & MantiCxrs - MONTAGEM MASENTADO.mp3"
+
+    ],
+poster:[
+   "https: //media4.giphy.com/media/v1.Y2lkPTZjMDliOTUyeWh6OTV1NnhtemN6djJ5eG51YzJkcjB1dGU1NXBscW8yamhzemU5eSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/4oMoIbIQrvCjm/200w.gif",
+   " https://media4.giphy.com/media/v1.Y2lkPTZjMDliOTUyeWh6OTV1NnhtemN6djJ5eG51YzJkcjB1dGU1NXBscW8yamhzemU5eSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/4oMoIbIQrvCjm/200w.gif",
+   "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGF2ZXl2Z3NuZ2NyYmlhdXhndnl4aWxud29pejV4cWFvZWR4bHZucyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/tqfS3mgQU28ko/200w.webp",
+   "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGF2ZXl2Z3NuZ2NyYmlhdXhndnl4aWxud29pejV4cWFvZWR4bHZucyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/wsWcsrfMXjJgk/100.webp"
+
+
+
+
+
+]
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
